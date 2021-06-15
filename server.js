250538cddf6f7870c9194ea5c81fa197ab7770a3
Ajay2908 <ajayreddyramapuram@gmail.com>
2021-06-15 11:43:55 +0530

    improving

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const bcrypt = require('bcrypt')
 const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 const userlogin = require('./routes/userlogin')
-const url = 'mongodb://localhost/userDB'
+dotenv.config();
+const url = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.znlfd.mongodb.net/dataDB`
 let currentuser;
-
+// console.log(url)
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 const con = mongoose.connection
 con.on('open', () => {
